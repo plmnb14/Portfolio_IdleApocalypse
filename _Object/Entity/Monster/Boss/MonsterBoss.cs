58e732cc09ab4fe31863495bdf473ee7e30375b2
Moontarious <55625018+plmnb14@users.noreply.github.com>
2025-08-17 01:34:12 +0900

    Add files via upload

---
+++ _Object/Entity/Monster/Boss/MonsterBoss.cs
@@ -0,0 +1,278 @@
+using System.Collections;
+using UnityEngine;
+using System.Linq;
+using CodeStage.AntiCheat.ObscuredTypes;
+
+public class MonsterBoss : Monster
+{
+    //----------------------------------------------------------------------------------------------------
+    // Fields
+    //----------------------------------------------------------------------------------------------------
+    #region Serialize Fields
+    [SerializeField] protected Transform cooldownSliderPosition;
+    #endregion
+
+    #region Property Fields
+    public ProjectileShooter ProjectileShooter
+    {
+        get { return _projectileShooter; }
+        private set { _projectileShooter = value; }
+    }
+    private ProjectileShooter _projectileShooter;
+    #endregion
+
+    #region Protected Fields
+    protected UISliderBarCooldown _cooldownSliderUI;
+    #endregion
+
+    //----------------------------------------------------------------------------------------------------
+    // Methods
+    //----------------------------------------------------------------------------------------------------
+    #region Awake Methods
+    private void Awake()
+    {
+        LoadComponents();
+        SetUpOnAwake();
+    }
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+
+        var projShooter = transform.Find("Projectile Shooter - VFX Play Point 0");
+        if(projShooter != null)
+            projShooter.TryGetComponent(out _projectileShooter);
+    }
+    #endregion
+
+    #region Start Methods
+    private void Start()
+    {
+        SetUpOnStart();
+    }
+
+    protected virtual void SetUpOnStart()
+    {
+        if (_projectileShooter != null)
+            _projectileShooter.Set_User(this);
+    }
+    #endregion
+
+    #region Die Methods
+    public override void KillForced(ObscuredBool isForced)
+    {
+        base.KillForced(isForced);
+
+        StopCoroutine(UpdateCooldownSliderUIPosition());
+
+        if (_cooldownSliderUI != null)
+            _cooldownSliderUI.BeginDie();
+
+    }
+    #endregion
+
+    #region Summoning Methods
+    public override void StartSummoning()
+    {
+        FindLongestDistanceAttack();
+        SetUpCooldownSliderUI();
+        UpdateCooldownSliderUI();
+
+        base.StartSummoning();
+    }
+
+    protected void FindLongestDistanceAttack()
+    {
+        var attackDBList = enemyDB_Server.attackDBLists[enemyDB_Cur.CurCombatPhase];
+        var value = attackDBList.Max(item => item.AttackRange);
+        var index = attackDBList.FindIndex(item => item.AttackRange == value);
+
+        enemyDB_Cur.CurAtkDBIdx = index;
+    }
+    #endregion
+
+    #region Cooldown Methods
+    protected void SetUpCooldownSliderUI()
+    {
+        _cooldownSliderUI = SliderBarUIManager.Instance.GetCooldownBarUI();
+        _cooldownSliderUI.MyRectTransform.position = cooldownSliderPosition.position;
+        _cooldownSliderUI.ActivateObject(false);
+    }
+
+    protected void UpdateCooldownSliderUI()
+    {
+        var curPhaseIndex = enemyDB_Cur.CurCombatPhase;
+        var curAtkIndex = enemyDB_Cur.CurAtkDBIdx;
+        var curAtkDB = enemyDB_Server.attackDBLists[curPhaseIndex][curAtkIndex];
+        _cooldownSliderUI.SetSliderValueMax(curAtkDB.Cooldown, 0);
+        _cooldownSliderUI.SetSliderValueCur(0.0f ,0);
+    }
+
+    private WaitForEndOfFrame _waitForFrame = new();
+    protected IEnumerator UpdateCooldownSliderUIPosition()
+    {
+        UpdateCooldownSliderUI();
+
+        while (_cooldownSliderUI.IsActivateComponenets && !InstanceEntityAbility.CanAutoAttack)
+        {
+            _cooldownSliderUI.MyRectTransform.position = cooldownSliderPosition.position;
+            _cooldownSliderUI.SetSliderValueCur(_curAttackCooldown, 0);
+
+            yield return _waitForFrame;
+        }
+
+        _cooldownSliderUI.gameObject.SetActive(false);
+        _cooldownSliderUI.ActivateObject(false);
+    }
+    #endregion
+
+    #region Attack Methods
+    public override void ActivateAttackVFX()
+    {
+        var actionCnt = enemyDB_Cur.CurActionCnt; 
+        var phaseIdx = enemyDB_Cur.CurCombatPhase;
+        var atkDBIdx = enemyDB_Cur.CurAtkDBIdx;
+
+        var atkDB = enemyDB_Server.attackDBLists[phaseIdx][atkDBIdx];
+
+        ObscuredVector2 curPosition = (Vector2)(atkDB.AttackMethodType == AttackMethodType.Melee 
+            ? (atkDB.VfxPositionIndexes.Length > 1 && VFXPlayPoints.Length > 1) 
+            ? VFXPlayPoints[atkDB.VfxPositionIndexes[actionCnt]].position 
+            : VFXPlayPoints[atkDB.VfxPositionIndexes[0]].position 
+            : atkDB.ActPositionType == ActPositonType.Target_Bottom 
+            ? InstanceEntityAbility.TargetEntityList[0].transform.position 
+            : InstanceEntityAbility.TargetEntityList[0].Get_LockOnPosition());
+
+        atkDB.ScaleRatio = InstanceEntityAbility.ScaleRatio;
+
+        if(atkDB.VfxNames[(int)ActionVFXType.Main].Length <= actionCnt)
+        {
+            --enemyDB_Cur.CurActionCnt;
+            --actionCnt;
+        }
+
+        var vfxMainString = atkDB.VfxNames[(int)ActionVFXType.Main][actionCnt];
+        var vfxMain = VFXManager.Instance.GetVFX(vfxMainString, curPosition, BackgroundManager.Instance.GetScrolledWorldTransform()) as VFXBattle;
+        vfxMain.transform.localScale *= atkDB.ScaleRatio;
+        vfxMain.SetUpStatus(this, atkDB, actionCnt);
+
+        PlayBossEnemySfx(ActionVFXType.Begin, phaseIdx, atkDBIdx, actionCnt);
+        vfxMain.OnCollide += () => { PlayBossEnemySfx(ActionVFXType.Main, phaseIdx, atkDBIdx, actionCnt); };
+
+        ++enemyDB_Cur.CurActionCnt;
+    }
+
+    protected override void StartAutoAttack()
+    {
+        InstanceEntityAbility.CanAutoAttack = false;
+        var curAtkDB = enemyDB_Server.attackDBLists[enemyDB_Cur.CurCombatPhase][enemyDB_Cur.CurAtkDBIdx];
+
+        _myAnimator.SetTrigger(curAtkDB.AnimationName);
+        if(curAtkDB.AttackMethodType == AttackMethodType.Range)
+        {
+            _projectileShooter.Set_AttackDB(curAtkDB);
+            _projectileShooter.ReadyToShoot(this, InstanceEntityAbility.TargetEntityList[0]);
+        }
+
+        enemyDB_Cur.CurActionCnt = 0;
+    }
+
+    public override void Finish_AutoAttack()
+    {
+        ReadyNextAttack();
+
+        base.Finish_AutoAttack();
+
+        _cooldownSliderUI.gameObject.SetActive(true);
+        _cooldownSliderUI.ActivateObject(true);
+        
+        StartCoroutine(UpdateCooldownSliderUIPosition());
+    }
+
+    public virtual void Attack_Automatically()
+    {
+        var beforePhase = enemyDB_Cur.CurCombatPhase;
+        var beforeAtkIdx = enemyDB_Cur.CurAtkDBIdx;
+        var beforeActCnt = enemyDB_Cur.CurActionCnt;
+
+        PlayBossEnemySfx(ActionVFXType.Begin, beforePhase, beforeAtkIdx, beforeActCnt);
+
+        _projectileShooter.Play_ShootEffect();
+        _projectileShooter.ShootProjectileAddAudioWithSpd(() =>
+        {
+            PlayBossEnemySfx(ActionVFXType.Main, beforePhase, beforeAtkIdx, beforeActCnt);
+        },
+        enemyDB_Server.attackDBLists[beforePhase][beforeAtkIdx].EnemyProjSpds[beforeActCnt]);
+
+        ++enemyDB_Cur.CurActionCnt;
+    }
+
+    public void PlayBossEnemySfx(ActionVFXType soundType, ObscuredInt phase, ObscuredInt atkCnt, ObscuredInt actionCnt)
+    {
+        var soundMgr = SoundManager.Instance;
+        var sfxNames = enemyDB_Server.attackDBLists[phase][atkCnt].SfxNames[(int)soundType];
+        var finalActionCnt = (int)(sfxNames.Length > actionCnt ? actionCnt : sfxNames.Length);
+
+        soundMgr.PlayEffectAudio(sfxNames[finalActionCnt]);
+    }
+
+    protected void ReadyNextAttack()
+    {
+        var maxAttackCount = enemyDB_Server.attackDBLists[enemyDB_Cur.CurCombatPhase].Count;
+        enemyDB_Cur.CurAtkDBIdx = GetRandomAttackIndex(0, maxAttackCount, enemyDB_Cur.CurAtkDBIdx);
+        enemyDB_Cur.CurActionCnt = 0;
+
+        var curAtkDB = enemyDB_Server.attackDBLists[enemyDB_Cur.CurCombatPhase][enemyDB_Cur.CurAtkDBIdx];
+        InstanceEntityAbility.SetAbilityValue(AbilityType_Instance.AtkDelay, (ObscuredDouble)curAtkDB.Cooldown);
+        InstanceEntityAbility.SetAbilityValue(AbilityType_Instance.AtkRange, (ObscuredDouble)curAtkDB.AttackRange);
+    }
+
+    private int GetRandomAttackIndex(ObscuredInt min, ObscuredInt max, ObscuredInt exceptIndex)
+    {
+        var randomRange = Enumerable.Range(min, max).Where(i => (i != exceptIndex));
+        int index = new System.Random().Next(0, max-1);
+
+        return randomRange.ElementAt(index);
+    }
+    #endregion
+
+    #region Pause Methods
+    public override void PauseActing()
+    {
+        base.PauseActing();
+
+        if(cooldownSliderPosition.gameObject.activeSelf)
+        {
+            StopCoroutine(UpdateCooldownSliderUIPosition());
+
+            _cooldownSliderUI.gameObject.SetActive(false);
+            _cooldownSliderUI.ActivateObject(false);
+        }
+    }
+    #endregion
+
+    #region Set Ailment
+    public override void SetAilment(AilmentType ailmentType, ObscuredBool isActive)
+    {
+        base.SetAilment(ailmentType, isActive);
+
+        switch (ailmentType)
+        {
+            case AilmentType.Stun:
+                {
+                    if (!isActive && !InstanceEntityAbility.isFrozen)
+                    {
+                        enemyDB_Cur.CurActionCnt = 0;
+                        enemyDB_Cur.CurAtkDBIdx = 0;
+
+                        UpdateInstanceAbility();
+                    }
+
+                    break;
+                }
+        }
+    }
+    #endregion
+}
+
+
---
+++ _Object/Entity/Monster/Boss/MonsterRaidBoss.cs
@@ -0,0 +1,177 @@
+using CodeStage.AntiCheat.ObscuredTypes;
+using System;
+
+public class MonsterRaidBoss : MonsterBoss
+{
+    //----------------------------------------------------------------------------------------------------
+    // Fields
+    //----------------------------------------------------------------------------------------------------
+    #region Protected Fields
+    protected ObscuredInt _curBerserkLevel;
+    protected ReinforceDB _curBerserkDB;
+    #endregion
+
+    //----------------------------------------------------------------------------------------------------
+    // Methods
+    //----------------------------------------------------------------------------------------------------
+    #region Awake Methods
+    private void Awake()
+    {
+        LoadComponents();
+        SetUpOnAwake();
+    }
+    #endregion
+
+    #region Start Methods
+    private void Start()
+    {
+        SetUpOnStart();
+        ResetBerserkAbility();
+    }
+    #endregion
+
+    #region Summon Methods
+    private UIHpBarBoss _bossHpBarUI;
+    private readonly ObscuredString _houndSummonAudioName = "Summon_Hound_00";
+    public override void StartSummoning()
+    {
+        if (StageManager.Instance.IsChangingMap) 
+            return;
+
+        CurEntityState = EntityState.Summoning;
+
+        _bossHpBarUI = null;
+
+        UpdateInstanceTarget();
+        UpdateInstanceAbility();
+
+        FindLongestDistanceAttack();
+        SetUpCooldownSliderUI();
+        UpdateCooldownSliderUI();
+
+        PlaySummoningVfx();
+        _myAnimator.SetTrigger("OnSummon");
+
+        SoundManager.Instance.PlayEffectAudio(_houndSummonAudioName);
+    }
+
+    public override void Finish_Summoning()
+    {
+        base.Finish_Summoning();
+
+        var dbList = DataManager.Instance.ReinforceDBDicts[Reinforce_Category.Enemy][(int)Reinforce_Group_Enemy.Raid][(int)Reinforce_Detail_Enemy_Raid.Berserk];
+
+        _bossHpBarUI = hpBar as UIHpBarBoss;
+        _bossHpBarUI.OnFloatValueUpdated += CheckPhaseByTime;
+        _bossHpBarUI.EndTimer += BeginDie;
+
+        _curBerserkDB = dbList[_curBerserkLevel];
+    }
+    #endregion
+
+    #region Die Methods
+    public override void KillForced(ObscuredBool isForced)
+    {
+        _curBerserkLevel = 0;
+
+        ResetBerserkAbility();
+
+        if (_bossHpBarUI != null && _bossHpBarUI.OnFloatValueUpdated != null)
+        {
+            _bossHpBarUI.OnFloatValueUpdated -= CheckPhaseByTime;
+            _bossHpBarUI = null;
+        }
+
+        base.KillForced(isForced);
+    }
+    #endregion
+
+    #region Ability Setting Methods
+    protected void SetBerserkAbility()
+    {
+        for (var i = 0; i < _curBerserkDB.reinforceAbilityDBs.Length; i++)
+        {
+            CurEntityAbility.SetAbilityValue(_curBerserkDB.reinforceAbilityDBs[i].abilityType, _curBerserkDB.reinforceAbilityDBs[i].default_Vlu, Ability_CalcValueType.Base);
+            CurEntityAbility.UpdateAbilityFinalValue(_curBerserkDB.reinforceAbilityDBs[i].abilityType);
+        }
+    }
+
+    protected void ResetBerserkAbility()
+    {
+        var db = DataManager.Instance.ReinforceDBDicts[Reinforce_Category.Enemy][(int)Reinforce_Group_Enemy.Raid][(int)Reinforce_Detail_Enemy_Raid.Berserk][0];
+
+        for(var i = 0; i < db.reinforceAbilityDBs.Length; i++)
+        {
+            CurEntityAbility.SetAbilityValue(db.reinforceAbilityDBs[i].abilityType, db.reinforceAbilityDBs[i].default_Vlu, Ability_CalcValueType.Base);
+            CurEntityAbility.UpdateAbilityFinalValue(db.reinforceAbilityDBs[i].abilityType);
+        }
+    }
+    #endregion
+
+    #region Check Methods
+    private const string BERSERK_GRADE = "±¤ÆøÈ­ {0}´Ü°è!!";
+    protected void CheckPhaseByTime(ObscuredFloat _timeValue)
+    {
+        var dbList = DataManager.Instance.ReinforceDBDicts
+            [Reinforce_Category.Enemy]
+            [(int)Reinforce_Group_Enemy.Raid]
+            [(int)Reinforce_Detail_Enemy_Raid.Berserk];
+
+        var nextBersetkLevel = _curBerserkLevel + 1;
+        if(dbList.Count > nextBersetkLevel)
+        {
+            if (dbList[nextBersetkLevel].reinforceUnlockDBs[0].reqVlu <= _timeValue)
+            {
+                _curBerserkLevel = nextBersetkLevel;
+                _curBerserkDB = dbList[_curBerserkLevel];
+                SetBerserkAbility();
+
+                if (_curBerserkLevel == 1)
+                {
+                    InstanceEntityAbility.IsSuperArmor = true;
+                    OnEndAtk = null;
+                    OnEndAtk += () => { ChangePhase(1); OnEndAtk = null; };
+                }
+
+                if (_curBerserkLevel == 4)
+                {
+                    OnEndAtk = null;
+                    OnEndAtk += () => { ChangePhase(2); OnEndAtk = null; };
+                }
+
+                MessageNotificationManager.Instance.ShowInstanceMessageNotification(
+                    string.Format(BERSERK_GRADE, _curBerserkLevel.ToString()));
+            }
+        }
+    }
+
+    protected void ChangePhase(ObscuredInt phaseIdx)
+    {
+        enemyDB_Cur.CurCombatPhase = phaseIdx;
+    }
+
+    private Action OnEndAtk;
+    public override void Finish_AutoAttack()
+    {
+        OnEndAtk?.Invoke();
+
+        base.Finish_AutoAttack();
+    }
+
+    protected void CancelCurrentActing()
+    {
+        StopCoroutine(StartAutoAttackCooldown());
+        StopCoroutine(UpdateCooldownSliderUIPosition());
+
+        _myAnimator.SetTrigger("OnReset");
+        _myAnimator.SetBool("IsRun", false);
+
+        _cooldownSliderUI.gameObject.SetActive(false);
+        _cooldownSliderUI.ActivateObject(false);
+        _curAttackCooldown = 0.0f;
+        InstanceEntityAbility.CanAutoAttack = true;
+
+        CurEntityState = EntityState.Idle;
+    }
+    #endregion
+}
